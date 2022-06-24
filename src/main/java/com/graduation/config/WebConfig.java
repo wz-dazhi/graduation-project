23a31e36f9bd24e766f0b501e3e9468276529779
wz-dazhi <742820195@qq.com>
2022-06-24 16:13:24 +0800

    web config

---
+++ src/main/java/com/graduation/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.graduation.config;
+
+import com.graduation.interceptor.UserInterceptor;
+import lombok.AllArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @projectName: graduation-project
+ * @package: com.graduation.config
+ * @className: WebConfig
+ * @description:
+ * @author: zhi
+ * @date: 2022/6/22
+ * @version: 1.0
+ */
+@Configuration
+@AllArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final UserInterceptor userInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry r) {
+        r.addInterceptor(userInterceptor).excludePathPatterns("/login.html", "/api/user/login");
+    }
+
+}
