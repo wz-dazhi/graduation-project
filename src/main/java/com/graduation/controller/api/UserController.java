f9774e9fdcce5e6b0abe4eb001fe8edb3f58d29b
wz-dazhi <742820195@qq.com>
2022-06-24 16:13:38 +0800

    add login

--- src/main/java/com/graduation/controller/api/UserController.java
+++ src/main/java/com/graduation/controller/api/UserController.java
@@ -2,7 +2,11 @@ package com.graduation.controller.api;
 
 import com.graduation.constant.Const;
 import com.graduation.service.UserService;
+import com.wz.swagger.model.Result;
+import com.wz.swagger.util.R;
+import com.wz.webmvc.util.WebContextUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    //@PostMapping("/login")
+    @GetMapping("/login")
+    public Result<Void> login() {
+        WebContextUtil.getSession().setAttribute("id", 1L);
+//        userService.login(null);
+        return R.ok();
+    }
+
+    @GetMapping("/quit")
+    public Result<Void> quit() {
+        WebContextUtil.getSession().invalidate();
+        return R.ok();
+    }
 }
