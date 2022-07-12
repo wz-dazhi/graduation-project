508819971aa914a37d504c4ac9af92ac32321bc9
wz-dazhi <742820195@qq.com>
2022-07-12 10:55:18 +0800

    add

---
+++ src/main/java/com/graduation/mapper/StatisticsMapper.java
@@ -0,0 +1,23 @@
+package com.graduation.mapper;
+
+import com.graduation.dto.resp.NameValueResp;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @projectName: graduation-project
+ * @package: com.graduation.mapper
+ * @className: StatisticsMapper
+ * @description:
+ * @author: zhi
+ * @date: 2022/7/11
+ * @version: 1.0
+ */
+public interface StatisticsMapper {
+    List<NameValueResp> income();
+
+    BigDecimal currentMonthTotal();
+
+    BigDecimal total();
+}
