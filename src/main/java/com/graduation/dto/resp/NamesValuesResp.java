a5dc2184e9708497e324aafccdd78ebb5ae808d8
wz-dazhi <742820195@qq.com>
2022-07-12 10:54:59 +0800

    add

---
+++ src/main/java/com/graduation/dto/resp/NamesValuesResp.java
@@ -0,0 +1,22 @@
+package com.graduation.dto.resp;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * @projectName: graduation-project
+ * @package: com.graduation.dto.resp
+ * @className: NameValueResp
+ * @description:
+ * @author: zhi
+ * @date: 2022/7/11
+ * @version: 1.0
+ */
+@Data
+public class NamesValuesResp implements Serializable {
+    private List<String> names;
+    private List<BigDecimal> values;
+}
