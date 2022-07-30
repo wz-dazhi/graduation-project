e80c7f4babdb170b1edc66375a80f42163e8e4dd
wz-dazhi <742820195@qq.com>
2022-07-30 14:03:39 +0800

    update

--- src/main/java/com/graduation/dto/resp/NameValueResp.java
+++ src/main/java/com/graduation/dto/resp/NameValueResp.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
  * @package: com.graduation.dto.resp
  * @className: NameValueResp
  * @description:
- * @author: zhi
+ * @author: yue
  * @date: 2022/7/11
  * @version: 1.0
  */
